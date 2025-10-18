d3a9cff2e7852c41e245fbde35a01814672b2bdd
TheRealAngelo <160996509+TheRealAngelo@users.noreply.github.com>
2025-10-18 23:19:18 +0800

    Update tagRoutes.js

--- src/routes/tagRoutes.js
+++ src/routes/tagRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const tagController = require('../controllers/tagController');
 
-router.post('/', tagController.createTag);
+router.get('/', tagController.listTags);   
+router.post('/', tagController.createTag);  
 
 module.exports = router;
